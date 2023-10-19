8584a1363195ebf84be58f00e92cdd44ab363010
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-10-20 00:42:35 +0600

    added guides section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                 <div className="bg-cover bg-center" style={{ backgroundImage: "url(/shapes.svg)" }}>
                     <div className="container mx-auto px-4">
                         <div className="hero min-h-[700px]">
-                            <div className="hero-content p-0 flex-col-reverse lg:flex-row-reverse min-h-[700px]">
+                            <div className="hero-content p-0 flex-col-reverse lg:flex-row-reverse h-full min-h-[700px]">
                                 <img src="https://i.ibb.co/nLVD4Ph/girl.png" className="lg:h-[700px]" />
                                 <div className="py-12 lg:py-0">
                                     <h1 className="text-xl font-semibold text-[#334155]">Just In! 🛍️</h1>
@@ -40,7 +40,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Clothing Collection
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Elevate Your Style with Our Collection.</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Elevate Your Style with Our Collection.</h4>
                                 <Link to="clothingCollection" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -54,7 +54,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Shoe Showcase
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Step into Style with Our Selection.</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Step into Style with Our Selection.</h4>
                                 <Link to="/shoeShowcase" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -68,7 +68,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Eyewear Collection
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">See Clearly, Look Stylish with Us</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">See Clearly, Look Stylish with Us</h4>
                                 <Link to="/eyewearCollection" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -84,7 +84,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Home & Kitchen Essentials
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Upgrade Your Space with Our Selection.</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Upgrade Your Space with Our Selection.</h4>
                                 <Link to="/homeKitchen" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -98,7 +98,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Electronics Hub
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Tech Up Your Life with Us.</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Tech Up Your Life with Us.</h4>
                                 <Link to="/electronics" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -112,7 +112,7 @@ const Home = () => {
                                 <span className="text-base font-semibold text-[#6B7280]">
                                     Gaming accessories
                                 </span>
-                                <h4 className="text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Level Up Your Gaming Experience Here.</h4>
+                                <h4 className="text-xl md:text-2xl font-semibold text-[#0F172A] flex-grow lg:mt-6">Level Up Your Gaming Experience Here.</h4>
                                 <Link to="/gamingAccessories" className="max-w-fit">
                                     <button className="btn bg-white text-[#0F172A] normal-case">Show me all</button>
                                 </Link>
@@ -161,8 +161,43 @@ const Home = () => {
                     }
                 </div>
             </div>
-
-
+            {/* Guides Section */}
+            <div className="container mx-auto px-4 py-[120px] border-t">
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8">
+                    <div className="text-center">
+                        <figure className="w-[120px] md:w-[150px] mx-auto mb-3">
+                            <img src="https://i.ibb.co/pfrRnSy/HIW1img-0a618bef4b830b5c682b.png" alt="" />
+                        </figure>
+                        <span className="py-1 px-2 bg-red-100 text-sm font-medium rounded-3xl">Step 1</span>
+                        <h1 className="text-base font-semibold text-[#0F172A] my-4">Filter & Discover</h1>
+                        <p className="text-sm font-medium text-[#6B7280] max-w-[250px] mx-auto">Smart filtering and suggestions make it easy to find</p>
+                    </div>
+                    <div className="text-center">
+                        <figure className="w-[120px] md:w-[150px] mx-auto mb-3">
+                            <img src="https://i.ibb.co/bgwGzDM/HIW2img-3d1e1bf2693a9272e96a.png" alt="" />
+                        </figure>
+                        <span className="py-1 px-2 bg-red-100 text-sm font-medium rounded-3xl">Step 2</span>
+                        <h1 className="text-base font-semibold text-[#0F172A] my-4">Add to bag</h1>
+                        <p className="text-sm font-medium text-[#6B7280] max-w-[250px] mx-auto">Easily select the correct items and add them to the cart</p>
+                    </div>
+                    <div className="text-center">
+                        <figure className="w-[120px] md:w-[150px] mx-auto mb-3">
+                            <img src="https://i.ibb.co/tBqK9mm/HIW3img-8b344c9a046e6b85a9ab.png" alt="" />
+                        </figure>
+                        <span className="py-1 px-2 bg-red-100 text-sm font-medium rounded-3xl">Step 3</span>
+                        <h1 className="text-base font-semibold text-[#0F172A] my-4">Fast shipping</h1>
+                        <p className="text-sm font-medium text-[#6B7280] max-w-[250px] mx-auto">The carrier will confirm and ship quickly to you</p>
+                    </div>
+                    <div className="text-center">
+                        <figure className="w-[120px] md:w-[150px] mx-auto mb-3">
+                            <img src="https://i.ibb.co/j8xYgnr/HIW4img-c6a12d19e09f27d32678.png" alt="" />
+                        </figure>
+                        <span className="py-1 px-2 bg-red-100 text-sm font-medium rounded-3xl">Step 4</span>
+                        <h1 className="text-base font-semibold text-[#0F172A] my-4">Enjoy the product</h1>
+                        <p className="text-sm font-medium text-[#6B7280] max-w-[250px] mx-auto">Have fun and enjoy your 5-star quality products</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
