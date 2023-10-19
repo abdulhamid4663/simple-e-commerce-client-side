65869a121128c7c8675aff71dc22d7a33562bbfc
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-10-20 01:25:13 +0600

    slightly changed in error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
                     <img src="/404.gif" />
                 </div>
                 <div className="text-center px-4">
-                    <h2 className="font-semibold text-[#68C0CE] mb-4 text-base lg:text-xl mb-2">We&apos;re sorry, but the page you are trying to access does not exist.</h2>
+                    <h2 className="font-semibold text-[#68C0CE] mb-4 text-base lg:text-xl lg:mb-2">We&apos;re sorry, but the page you are trying to access does not exist.</h2>
                     <h1 className="font-semibold text-[#68C0CE] text-base lg:text-xl mb-6">We kindly ask you to return to the homepage</h1>
                     <Link to="/">
                         <button className="btn bg-[#1687A1] text-white hover:text-[#0F172A] rounded-full">Homepage</button>
